cc0a2bd8afbc6671428034878728d9f5c732897a
Shivam-Chaurasia17 <shivamchaurasia6570@gmail.com>
2025-08-29 13:46:24 +0000

    datatypes

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict";  // use all JS code as newer version
+
+//alert(3+3) will not run and show error as we are using node js js not browser
+
+
+let name = "Shivam" //string
+let age = 19  // integer/numbers ->2 to power53 /bigint
+let isLoggedIn = false/true //boolean
+
+
+//null -> standalone value // can say the value we still has't got 
+
+//undefined ->  means the value is not defined yet
+
+//symbol -> unique
+
+//object
+
+console.log(typeof undefined); //type of undefined is undefined
+console.log(typeof null);// type of null is object 
