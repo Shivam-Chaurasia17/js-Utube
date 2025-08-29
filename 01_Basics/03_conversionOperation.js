4b0221b9237ca456467fced12b34d3de336cfebe
Shivam-Chaurasia17 <shivamchaurasia6570@gmail.com>
2025-08-29 14:53:41 +0000

    conversions and operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,18 +1,38 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"26634" -> 26634
 //"2356aa0"-> NaN
 //true -> 1 and false -> 0
 
-let isLoggedIn =1
+let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn) 
+// console.log(booleanIsLoggedIn)
 
-let booleanIsLoggedIn = Boolean(isLoggedIn) 
-console.log(booleanIsLoggedIn)
+
+
+//************************ Operations ************************
+
+let value = 35
+let negValue = -value 
+// console.log(negValue);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
