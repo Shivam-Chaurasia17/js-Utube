69d53b34749c6d396700fb3426a48983f2feb0c4
Shivam-Chaurasia17 <shivamchaurasia6570@gmail.com>
2025-08-29 14:10:03 +0000

    conversion and operstions

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"26634" -> 26634
+//"2356aa0"-> NaN
+//true -> 1 and false -> 0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+console.log(booleanIsLoggedIn)
