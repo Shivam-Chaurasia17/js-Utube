b18f08c424697a77e0309538e9bb7dc3f6546e30
Shivam-Chaurasia17 <shivamchaurasia6570@gmail.com>
2025-08-30 19:32:56 +0000

    Array

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,34 @@
+// Arrays 
+ 
+const myArr = [1,2,3,4,5,6]
+// const myArr2 = new Array(1,2,3,4,5,6)
+// console.log(myArr[5]);
+
+
+//Array methods
+
+// myArr.push(6) // add this number to the back of the array 
+// console.log(myArr);
+// myArr.pop()// remove/ delete last element 
+
+// myArr.unshift(42)// add this number to the begining of the array
+// myArr.shift()// remove the first element of the array
+
+// console.log(myArr.includes(102));
+// console.log(myArr.indexOf(102));
+
+// const newArr = myArr.join()/// copy the elements of previous array into new one and type cast the new array into string
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr); // string
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1,3);//does't manupulate the original array 
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1,3);//manupulates the original array as the porsition mentioned will get deleted
+// console.log(myn2);
+// console.log("C ", myArr);
