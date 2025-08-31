fad1a754e719100b0f309dd56f7080641f5278ab
Shivam-Chaurasia17 <shivamchaurasia6570@gmail.com>
2025-08-31 19:46:24 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -4,12 +4,44 @@
 // }
 // sum(2,5)
 
-function userLoggedin(username){
-    if(!username){
+function userLoggedin(username) {
+    if (!username) {
         console.log("plz enter a username")
         return
     }
     return `${username} just logged in`
 }
 
-console.log(userLoggedin());
+// console.log(userLoggedin());
+
+function calculateCartprice(...num1) {
+    return num1
+
+}
+
+// console.log(calculateCartprice(200,300,26))
+
+const user = {
+    username: "Shivam",
+    age: 19
+}
+
+function handleusername(anyobject) {
+    console.log(`username is ${anyobject.username} and their age is ${anyobject.age}`)
+}
+
+// handleusername(user)
+
+// handleusername({
+//     username: "Harry",
+//     age: 20
+// })
+
+const myNewArray = [100, 200, 652, 69]
+
+
+function retursecondvalue(getArray) {
+    return getArray[1]
+}
+
+console.log(retursecondvalue(myNewArray));
